db91fded03ee35caa140a083e9a690bbf6a1c987
Sertaç Can <82063025+SertacN@users.noreply.github.com>
2025-12-07 00:57:53 +0300

    standart config for maints

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  await app.listen(configService.get('PORT') ?? 3000);
 }
 bootstrap();
