9cdb39b1a9914f38323a1222926b1f73f2c5a9f8
Sertaç Can <82063025+SertacN@users.noreply.github.com>
2025-12-07 01:34:18 +0300

    implement prisma orm and pg db

---
+++ api/prisma/migrations/20251206221637_mig_1/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "refreshToken" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
