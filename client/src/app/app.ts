e722f17c66ff6a7443c9820b128ca897abab3fe8
Sertaç Can <82063025+SertacN@users.noreply.github.com>
2025-12-06 23:39:04 +0300

    implement api for client (dev)

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient, httpResource } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('client');
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:3000/';
+  getData(): Observable<any> {
+    return this.http.get<any>(this.apiUrl).pipe(
+      tap((response) => {
+        console.log(response);
+      })
+    );
+  }
+  data = toSignal(this.getData());
 }
